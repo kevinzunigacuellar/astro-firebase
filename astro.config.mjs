29e869570889fe81295d9cf09d8b0b40f2bb598c
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-03-23 02:10:04 -0400

    switch adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
-import netlify from "@astrojs/netlify/functions";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
