3c7058fc0b277c89b0b4fefe72452ebd9d5323bd
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-03-27 16:07:48 -0400

    idk if catch will actually catch this errors

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,13 +2,41 @@ import type { APIRoute } from "astro";
 import firebase from "../../lib/firebase/server";
 
 export const post: APIRoute = async ({ redirect, request, cookies }) => {
-  const { idToken } = await request.json();
-  const expiresIn = 60 * 60 * 24 * 5 * 1000;
+  /* Get the ID token */
+  const { idToken } = await request.json().catch((error) => {
+    return new Response(
+      JSON.stringify({
+        message: error.message,
+      }),
+      { status: 401 }
+    );
+  });
+
+  /* Verify the ID token */
+  await firebase.auth().verifyIdToken(idToken).catch((error) => {
+    return new Response(
+      JSON.stringify({
+        message: error.message,
+      }),
+      { status: 401 }
+    );
+  })
+
+  const fiveDays = 60 * 60 * 24 * 5 * 1000;
   const sessionCookie = await firebase
     .auth()
-    .createSessionCookie(idToken, { expiresIn });
+    .createSessionCookie(idToken, { expiresIn: fiveDays }).catch((error) => {
+      return new Response(
+        JSON.stringify({
+          message: error.message,
+        }),
+        { status: 401 }
+      );
+    });
+
   cookies.set("session", sessionCookie, {
     path: "/",
   });
+
   return redirect("/dashboard", 302);
 };
