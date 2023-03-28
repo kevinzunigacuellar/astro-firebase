f5e7f950261bdb33688860047b6cfef26ac9929b
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-03-28 17:13:24 -0400

    style sign in with google button

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -7,7 +7,13 @@ import {
   Match,
 } from "solid-js";
 import { loginSchema } from "../lib/schemas";
-import { signInWithEmailAndPassword, inMemoryPersistence, getAuth } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  inMemoryPersistence,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { app } from "../lib/firebase/client";
 import ErrorPlaceholder from "./ErrorPlaceholder";
 import Error from "./Error";
@@ -15,21 +21,44 @@ import type { z } from "zod";
 
 type Errors = z.typeToFlattenedError<z.inferFormattedError<typeof loginSchema>>;
 type SucessForm = z.infer<typeof loginSchema>;
+const auth = getAuth(app);
+/* This will set the persistence to session */
+auth.setPersistence(inMemoryPersistence);
 
 async function postFormData(formData: SucessForm) {
   const { email, password } = formData;
-  const auth = getAuth(app)
-  
-  /* This will set the persistence to browser session */
-  const userCredential = await auth.setPersistence(inMemoryPersistence).then(() => signInWithEmailAndPassword(
+  const userCredential = await signInWithEmailAndPassword(
     auth,
     email,
     password
-  ))
+  );
   const idToken = await userCredential.user.getIdToken();
   const res = await fetch("/api/login", {
-    method: "POST",
-    body: JSON.stringify({ idToken }),
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+  });
+
+  if (!res.ok) {
+    const data = await res.json();
+    return data;
+  }
+
+  if (res.redirected) {
+    window.location.assign(res.url);
+  }
+}
+
+async function googleSignIn() {
+  const provider = new GoogleAuthProvider();
+  const userCredential = await signInWithPopup(auth, provider);
+  const idToken = await userCredential.user.getIdToken();
+  const res = await fetch("/api/login", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
   });
 
   if (!res.ok) {
@@ -105,6 +134,18 @@ export default function LoginForm() {
         <Show fallback="Sign in" when={response.loading}>
           Signing in...
         </Show>
+      </button>
+      <div>
+        <hr class="h-0 border-t mt-4 border-zinc-600"></hr>
+        <p class="-mt-2.5 text-sm text-center text-zinc-400"><span class='bg-zinc-900 px-4'>Or with</span></p>
+      </div>
+      <button
+        onclick={googleSignIn}
+        class="bg-zinc-100 py-1.5 border border-zinc-100 flex justify-center items-center gap-2 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
+        type="button"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5" preserveAspectRatio="xMidYMid" viewBox="0 0 256 262"><path fill="#4285F4" d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"/><path fill="#34A853" d="M130.55 261.1c35.248 0 64.839-11.605 86.453-31.622l-41.196-31.913c-11.024 7.688-25.82 13.055-45.257 13.055-34.523 0-63.824-22.773-74.269-54.25l-1.531.13-40.298 31.187-.527 1.465C35.393 231.798 79.49 261.1 130.55 261.1"/><path fill="#FBBC05" d="M56.281 156.37c-2.756-8.123-4.351-16.827-4.351-25.82 0-8.994 1.595-17.697 4.206-25.82l-.073-1.73L15.26 71.312l-1.335.635C5.077 89.644 0 109.517 0 130.55s5.077 40.905 13.925 58.602l42.356-32.782"/><path fill="#EB4335" d="M130.55 50.479c24.514 0 41.05 10.589 50.479 19.438l36.844-35.974C195.245 12.91 165.798 0 130.55 0 79.49 0 35.393 29.301 13.925 71.947l42.211 32.783c10.59-31.477 39.891-54.251 74.414-54.251"/></svg>
+        <span>Sign in with Google</span>
       </button>
       <Suspense>
         <Switch>
