262221ce818ecd438a94bb6c6497a0147db7b84a
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-03-27 21:37:27 -0400

    fix: delete cookie on sign out

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const post: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete("session");
+  cookies.delete("session", {
+    path: "/",
+  });
   return redirect("/login", 302);
 };
