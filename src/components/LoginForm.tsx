1829f641e3436916e7e385d180d6b83d8b76da86
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-03-30 22:31:32 -0400

    addjust padding in login buttons

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -151,7 +151,7 @@ export default function LoginForm() {
         </Show>
       </div>
       <button
-        class="bg-zinc-100 py-2 border border-zinc-100 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
+        class="bg-zinc-100 py-1.5 border border-zinc-100 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
         type="submit"
         disabled={response.loading}
       >
@@ -168,7 +168,7 @@ export default function LoginForm() {
       <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
         <button
           onclick={googleSignIn}
-          class="bg-zinc-100 p-2 border border-zinc-100 flex justify-center items-center gap-2 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
+          class="bg-zinc-100 p-1.5 border border-zinc-100 flex justify-center items-center gap-2 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
           type="button"
         >
           <svg
@@ -198,7 +198,7 @@ export default function LoginForm() {
         </button>
         <button
           onclick={githubSignIn}
-          class="bg-zinc-100 p-2 border border-zinc-100 flex justify-center items-center gap-2 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
+          class="bg-zinc-100 p-1.5 border border-zinc-100 flex justify-center items-center gap-2 rounded-md mt-2 text-black font-medium text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-600 focus:ring-offset-zinc-900 disabled:opacity-50 disabled:cursor-not-allowed"
           type="button"
         >
           <svg
